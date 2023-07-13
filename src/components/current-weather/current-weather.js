9682045d4ef853927626db4c48538fa58a27839d
Siwi0w0 <yufei.sun64@gmail.com>
2023-07-13 23:55:43 +0100

    Update forecast component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -15,26 +15,26 @@ const CurrentWeather = ({ data }) => {
         />
       </div>
       <div className="bottom">
-        <p className="temperature">15°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-lable">Details</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-lable">Feels like</span>
-            <span className="parameter-value">13°C</span>
+            <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-lable">Wind</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-lable">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-lable">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
