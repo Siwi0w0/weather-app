5dc9aa05af5fc215703c4c55732de00808328579
Siwi0w0 <yufei.sun64@gmail.com>
2023-07-24 10:55:02 +0100

    Add default weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
+import DefaultWeather from "./components/default-weather/default-weather";
 import Forecast from "./components/forecast/forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import { useState } from "react";
@@ -8,6 +9,7 @@ import { useState } from "react";
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [defaultWeather, setDefaultWeather] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -35,6 +37,7 @@ function App() {
 
   console.log(currentWeather);
   console.log(forecast);
+  console.log(defaultWeather);
 
   return (
     <>
@@ -42,14 +45,15 @@ function App() {
         <Search onSearchChange={handleOnSearchChange} />
         {currentWeather && <CurrentWeather data={currentWeather} />}
         {forecast && <Forecast data = {forecast} />}
+        {defaultWeather && <DefaultWeather data = {defaultWeather} />}
       </div>
       <div className = "footer-info">
         <a href="https://github.com/Siwi0w0/weather-app">
             Source Code
         </a>{" "}
           | Developed by{" "}
-          <a target="_blank" href="https://github.com/Siwi0w0">
-            Yufei S
+          <a target="_blank" rel="noreferrer" href="https://github.com/Siwi0w0">
+            Yufei Sun
           </a>{" "}
       </div>
     </>
